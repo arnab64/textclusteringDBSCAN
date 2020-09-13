615fa72fd28db96f9deed6eebb1ce3f5a23b8c1d
Arnab Mac <arnabborah@Arnabs-Mac-mini.local>
2020-09-13 00:04:37 -0700

    structuring categorical characteristic module

---
+++ scripts/1_categoricalCharacteristicModule.py
@@ -0,0 +1,8 @@
+import fasttext
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import nltk,re,pprint
+import sys,glob,os
+from imp import reload
+from nltk.corpus import stopwords
