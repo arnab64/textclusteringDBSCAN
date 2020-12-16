16dc8cb29964ebca53b05af2e70390f798375d07
Mykola Maliarenko <mykola.maliarenko@gmail.com>
2020-12-16 04:40:47 +0200

    added suport of sentence-transfromers pretrained vectorizers

--- textclustering/encoders.py
+++ textclustering/encoders.py
@@ -1,34 +1,24 @@
 from sentence_transformers import SentenceTransformer
-# import tensorflow_hub as hub
-from src.config import Config
 import numpy as np
 
-config = Config()
-
-class Encoders():
-    '''
-    Class containing diferent sentence encoder options.
-    '''
-    def __init__(self, batchSize=8):
-        self.bertEncoder = SentenceTransformer(config.bertVersionDefault)
+class BertEncoder():
+    ''' Class containing diferent sentence encoder options '''
+    def __init__(self, batchSize: int=8, version: str='distilbert-base-nli-stsb-mean-tokens'):
+        self.bertEncoder = SentenceTransformer(version)
         self.batchSize = batchSize
 
-    def setTransformerVersion(self, version):
-        '''
-        Changes the transformer version used for encoding.
+    def set_transformer_version(self, version: str):
+        ''' Changes the transformer version used for encoding.
         Version options:
         'distilbert-base-nli-stsb-mean-tokens'
         'bert-large-nli-stsb-mean-tokens'
-        'bert-large-nli-mean-tokens'
-        '''
+        'bert-large-nli-mean-tokens' '''
 
         self.bertEncoder = SentenceTransformer(version)
 
-    def bert_encode(self, sentences):
-        '''
-        Encodes the list of sentences with BERT encoder.
-        Returns the list of vectors.
-        '''
+    def bert_encode(self, sentences: list):
+        ''' Encodes the list of sentences with BERT encoder.
+        Returns the list of vectors '''
 
         return np.stack(self.bertEncoder.encode(sentences,
                                                 self.batchSize,
